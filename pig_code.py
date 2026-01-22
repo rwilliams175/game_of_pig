61402fb35e45cec155cad8e8905eb2e0728f3b40
rwilliams175 <rwilliams175@phm.k12.in.us>
2026-01-22 09:46:34 -0500

    Update pig_code.py

--- pig_code.py
+++ pig_code.py
@@ -2,14 +2,23 @@ import random
 win = 100
 
 def dice():
-    random.randint(1,6)
+    return random.randint(1,6)
 
 def turn():
-    turn_count = 0
+    turn_score = 0
     while True:
         roll = dice()
-        print("You rolled a " + roll)
+        print("You rolled a " + str(roll))
         if roll == 1:
             print("You busted! Back to 0")
-            
-            turn_count +=
+            return 0
+        turn_score += roll
+        print("Turn total: " + str(turn_score))
+        
+        choice = input("Would you like to roll or stay (r/s)?")
+        if choice == "s":
+            print("You have chosen to stay, your total for this turn is " + str(turn_score))
+            return turn_score
+
+def play_game():
+    
